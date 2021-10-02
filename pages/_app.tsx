cfae45a57dcb89404cd9b7c7d1dadc98d0762c5a
Lleyton Gray <lleyton@innatical.com>
2021-10-02 11:30:33 -0700

    Move out BrowserRouter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.scss";
 import Routes from "inn:routes";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
