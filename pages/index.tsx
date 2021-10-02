b46f380a670e988059a6fbc53deb2b4ae4bc16bd
Lleyton Gray <lleyton@innatical.com>
2021-10-02 08:47:27 -0700

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome to Inn.ts!</h1>
+      <p>An awesome framework for making React apps by Innatical.</p>
+
+      <div className="links">
+        <a href="https://innts.innatical.com">Inn.ts</a>
+        <a href="https://innts.innatical.com/docs">Documentation</a>
+        <a href="https://github.com/innatical/inn.ts">Fork us on GitHub!</a>
+      </div>
+    </>
+  );
+};
+
+export default Home;
