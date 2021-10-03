d157c4eceed9ec5faf9e274ac21604373f74b806
Lleyton Gray <lleyton@innatical.com>
2021-10-02 19:09:49 -0700

    Add MDX Support
    Co-authored-by: infinitebash <infinitebash@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
 const Home = () => {
   return (
     <>
+      <div style={{ display: "flex" }}>
+        <img
+          src="/innatical.svg"
+          alt="Innatical Logo"
+          width="50"
+          height="50"
+          style={{ marginLeft: "auto", marginRight: "auto" }}
+        />
+      </div>
       <h1>Welcome to Inn.ts!</h1>
       <p>An awesome framework for making React apps by Innatical.</p>
 
